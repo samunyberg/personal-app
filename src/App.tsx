3c6c2b81ed7c8dd6d9622337e8c41bb085c1b0db
samunyberg <samu.nyberg@gmail.com>
2024-06-19 12:57:45 +0300

    Change app CSS to CSS modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import "./App.css";
+import styles from "./App.module.css";
 import Header from "./components/header/Header";
 import Slider from "./components/slider/Slider";
 
 function App() {
   return (
-    <div className="app">
+    <div className={styles.app}>
       <Header />
-      <div className="app-projects">
-        <h2 className="app-heading">Projects</h2>
+      <div className={styles.projects}>
+        <h2 className={styles.heading}>Projects</h2>
         <Slider />
       </div>
     </div>
